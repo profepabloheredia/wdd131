660f270793facc5efec6f091d52bb58baa1587b1
profepabloheredia <profepabloheredia@gmail.com>
2024-09-30 16:07:15 -0300

    Temple Files Created

--- week02/Activities/loops_activities/loops.js
+++ week02/Activities/loops_activities/loops.js
@@ -19,22 +19,46 @@ myInfo = {
       },
     ],
   };
-  // Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
+
+  const foodElements= document.querySelector('#favorite-foods');
+  function createFooditem(food){
+    let favoriteFood = document.createElement('li');
+    favoriteFood.textContent=food;
+    foodElements.appendChild(favoriteFood);
+  }
+  myInfo.favoriteFoods.forEach(createFooditem);  
+ 
+  /* Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
   let favoriteFood1 = document.createElement("li");
   favoriteFood1.textContent = myInfo.favoriteFoods[0];
   
   let favoriteFood2 = document.createElement("li");
   favoriteFood2.textContent = myInfo.favoriteFoods[1];
-  
+
+   
   let favoriteFood3 = document.createElement("li");
   favoriteFood3.textContent = myInfo.favoriteFoods[2];
   
   let favoriteFood4 = document.createElement("li");
   favoriteFood4.textContent = myInfo.favoriteFoods[3];
   
+
+  
   // Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
   document.querySelector("#favorite-foods").appendChild(favoriteFood1);
   document.querySelector("#favorite-foods").appendChild(favoriteFood2);
   document.querySelector("#favorite-foods").appendChild(favoriteFood3);
-  document.querySelector("#favorite-foods").appendChild(favoriteFood4);
-
+  document.querySelector("#favorite-foods").appendChild(favoriteFood4);*/
+  const DAYS = 6;
+  const LIMIT = 30;
+  let studentReport = [11, 42, 33, 64, 29, 37, 44];
+for (let i=0; i<studentReport.length;i++){
+  if (studentReport[i] < LIMIT){
+    console.log(studentReport[i]);
+  }
+}
+for (let i = 0; i < studentReport.length; i++) {
+  if (studentReport[i] < LIMIT) {
+    console.log(studentReport[i]);
+  }
+}
--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,25 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector("ul");
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent= '❌';
-li.append(deleteButton);
-list.append(li);
+
+document.button.addEventListener('click', function() {
+    // Code to execute when the element is clicked
+    if (input.value.trim() == ''){input.focus();}
+    else{
+        const input = document.querySelector('#favchap');
+        const button = document.querySelector('button');
+        const list = document.querySelector("#list");
+
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent= '❌';
+        li.append(deleteButton);
+        list.append(li);
+    }
+  });
+  deleteButton.addEventListener('click', function() {
+    // Code to execute when the element is clicked
+    list.removeChild(li);
+    input.focus();
+    }
+  );
+  input.value='cassa';
+  input.focus();
