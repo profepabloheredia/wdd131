8fdda822b7d3e21f99b1691240f1c5ff1fa6718c
profepabloheredia <profepabloheredia@gmail.com>
2024-10-09 22:50:01 -0300

    .

--- scripts/place.js
+++ scripts/place.js
@@ -1,7 +1,4 @@
-
-
-
-function calculateWindChill(temp, windSpeed){
+function calculateWindChill(temp, windSpeed){// the temp must to be in fahrenheit
 
     if (temp <= 10 && windSpeed > 4.8) {
         let t = temp;
@@ -16,7 +13,7 @@ function calculateWindChill(temp, windSpeed){
 const celsius = 5;//Celcius degree
 const windSpeed = 10;// km/h
 
-const temp = (celsius * (9/5)) + 32;//fahrenheit
+const temp = (celsius * (9/5)) + 32;// covert to fahrenheit
 
 const windChill= calculateWindChill(temp, windSpeed);
 document.getElementById('wind-Chill').textContent = typeof windChill === 'number' ? windChill.toFixed(1) + "°C" : windChill;
@@ -24,23 +21,3 @@ document.getElementById('wind-Chill').textContent = typeof windChill === 'number
 //footer content
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
-
-
-   
-    
-
-
-// def wind_chill(temperature, wind): # calculate and return the wind chill value
-//     t = temperature
-//     v = wind
-//     wind_chill_value = 35.74 + (0.6215 * t) - (35.75* (v ** 0.16)) +(0.4275 * t * (v ** 0.16))
-//     wind_chill_value
-//     return wind_chill_value
-
-// def convert_celsius_fahrenheit (celsius):
-//     fahrenheit = (celsius * (9/5)) + 32
-//     return fahrenheit
-
-// def convert_fahrenheit_celsius (fahrenheit):
-//     celsius = (fahrenheit - 32) / (9/5)
-//     return celsius
