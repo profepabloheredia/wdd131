fab730f45f30ff8a6b345af0c6890b5390895d7a
profepabloheredia <profepabloheredia@gmail.com>
2024-10-23 21:25:59 -0300

    Project files creation

---
+++ project/scripts/bmi-project.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//footer content
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
